10a8123f5e62e0ef034383145ff6832e61cb4af7
MadsHin <47994373+MadsHin@users.noreply.github.com>
2023-09-19 19:14:41 +0000

    Assignment without Github Done

--- Assignment01/airport_finder.py
+++ Assignment01/airport_finder.py
@@ -1,5 +1,5 @@
 import math
-from functools import reduce
+import os
 
 class AirportFinder:
 
@@ -9,7 +9,9 @@ class AirportFinder:
 
 
     def optimal_airport_hub(self, airports : list[tuple[float, float]]) -> tuple[float, float]:
-        
+        """
+        finds the best airport for a set of inputs
+        """
         candidates = list()
 
         for airport_candidate in airports:
@@ -27,21 +29,26 @@ class AirportFinder:
 
         return sorted(candidates, key=lambda x: float(x[1]))[0][0]
                  
-    def calculate_distance(self, a: tuple[float, float], b: tuple[float, float]) -> float:
-        return math.sqrt((b[0]-a[0])**2+(b[1]-a[1])**2)
+    def calculate_distance(self, point_a: tuple[float, float], point_b: tuple[float, float]) -> float:
+        """finds the euclidean norm"""
+        return math.sqrt((point_b[0]-point_a[0])**2+(point_b[1]-point_a[1])**2)
 
     def convert_string_to_tuple(self, input_string: str) -> tuple[float, float]:
+        """simple conversion from string to tuple"""
         return tuple(map(float, input_string.split(' ')))
 
     def find_airport_hubs(self) -> list[(float, float)]:
-     
+        """
+        Responsible for finding each set of problems in the file
+        and returning the list, and make an solution.ans file
+        """     
         line_number : int = 0
         airport_hub_list : list[tuple[float, float]] = list()
 
         while line_number < len(self.airports_input):
 
             try:
-                number_of_airports = int(self.airports_input[line_number])
+                number_of_airports = int(self.airports_input[line_number][0])
 
             except IndexError:
                 break
@@ -50,6 +57,12 @@ class AirportFinder:
             line_number += number_of_airports+1
             airport_hub_list.append(self.optimal_airport_hub(airports))
 
-            
-
+        self.output_to_file(airport_hub_list)
         return airport_hub_list
+
+    def output_to_file(self, result_list : list[tuple[float, float]]) -> None:
+        with open("outputfile.ans", "w", encoding="utf8") as file:
+            for line in result_list:
+                file.write(str(line[0]) + " " + str(line[1]) + "\n")
+    
+
--- Assignment01/tests.py
+++ Assignment01/tests.py
@@ -1,6 +1,12 @@
 import pytest
-
 from airport_finder import AirportFinder
+
+"""
+To run the program, run "pytest <path_to_this_file>
+In addition to the tests, there will be created a file called outputfile.ans,
+with the same format as the assignment told me to.
+"""
+
 
 test_input_path = "/workspaces/copilot-effeciency-study-pilot-ii-MadsHin/Assignment01/airlinehub.in"
 test_output_path = "/workspaces/copilot-effeciency-study-pilot-ii-MadsHin/Assignment01/airlinehub.ans"
@@ -24,5 +30,5 @@ def test_optimal_airport_hub(airport_finder_sut):
 
 
 def test_find_airport_hubs(airport_finder_sut):
-    assert airport_finder_sut.find_airport_hubs() == list[(3.20,-15.00),(3.20,-15.00)]
+    assert airport_finder_sut.find_airport_hubs() == [(3.20,-15.00),(3.20,-15.00)]
 
