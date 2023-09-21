d56f61ddf79ae727d6312310e444b0b8c31cb702
MadsHin <47994373+MadsHin@users.noreply.github.com>
2023-09-21 21:31:43 +0000

    Finished with assignment with copilot

--- Assignment02/pattern_matching.py
+++ Assignment02/pattern_matching.py
@@ -27,6 +27,70 @@ def is_int(line):
     else:
         return False
 
-# Function that divides the list into sublists based on the integers in the list and returns the sublists
+def get_problems(line_list):
+    # Creating an empty list to store the problems
+    problems = []
+    # Looping through the lines in the list
+    for i, line in enumerate(line_list):
+        # If the line is an integer then store the next lines in a list
+        if is_int(line):
+            # Creating an empty list to store the next lines
+            problem = line_list[i+1:i+int(line)+2]
+            
+            # Appending the list to the problems list
+            problems.append(problem)
+    
+        
+        
+    
+    # Returning the problems list
+    return problems
+
+# function that solves the problems
+
+def solve_problem(file_name):
+
+    #clear the file
+    open("output.ans", "w", encoding="utf8").close()
+
+    lines = read_file(file_name)
+
+    problems_list = get_problems(lines)
+
+    for problem in problems_list:
+
+        #Set the last value in the list as the string to search in
+        string_to_search_in = problem[-1].strip()
+
+        phrases_to_search_for = [phrase.strip() for phrase in problem[:-1]]
+
+        for phrase in phrases_to_search_for:
+            # create a list to store the indexes of the found words
+            index_list = []
+            # loop through the string to search in
+            for i in range(len(string_to_search_in)):
+                # if the phrase is found in the string, append the index to the list
+                if string_to_search_in[i:i+len(phrase)] == phrase:
+                    index_list.append(i)
+
+                        
+            # write the indexes to a file on a single line with a space between each index
+            # if the word is not found, write \n to the file
+            # the line cannot end with a space. So the last index in the list is written without a space
+
+            if len(index_list) > 0:
+                with open("output.ans", "a", encoding="utf8") as file:
+                    for index in index_list[:-1]:
+                        file.write(str(index) + " ")
+                    file.write(str(index_list[-1]) + "\n")
+            else:
+                with open("output.ans", "a", encoding="utf8") as file:
+                    file.write("\n")
+            
+
+            
 
 
+solve_problem("/workspaces/copilot-effeciency-study-pilot-ii-MadsHin/Assignment02/stringmultimatching.in")
+        
+        
--- Assignment02/tests.py
+++ Assignment02/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from pattern_matching import is_int, read_file
+from pattern_matching import is_int, read_file, get_problems, solve_problem
 
 
 def test_is_int():
@@ -21,3 +21,13 @@ def test_is_int():
     assert is_int("1 1 1 1 1 1 1 1 1 1 1 1 1") == False
     assert is_int("1 1 1 1 1 1 1 1 1 1 1 1 1 1") == False
 
+
+test_list = ["3","n","hej", "dav", "dette er min streng", "3", "n", "hej", "dav", "dette er min streng2", "3", "n", "hej", "dav", "dette er min streng3"]
+def test_get_problems():
+    assert get_problems(test_list) == [["n","hej", "dav", "dette er min streng"], ["n", "hej", "dav", "dette er min streng2"], ["n", "hej", "dav", "dette er min streng3"]]
+
+
+def test_solve_problem():
+    # assert content of the file "stringmultimatching.in" is equal to the file "output.txt"
+    solve_problem("stringmultimatching.in")
+    assert read_file("output.ans") == read_file("stringmultimatching.ans")
